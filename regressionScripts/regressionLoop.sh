50ea3691cc52b79a9ee8db16ec2a2d42bd6e7b2b
Christos Papageorgakis <Christos.Papageorgakis@cern.ch>
2020-10-19 14:46:02 -0400

    Fix typo

--- regressionScripts/regressionLoop.sh
+++ regressionScripts/regressionLoop.sh
@@ -3,9 +3,9 @@
 energies=(10 50 100 200 300 500)
 deadFractions=(0 1 3 5 7)
 
-for e in energies
+for e in ${energies[@]}
 do
-for df in deadFractions
+for df in ${deadFractions[@]}
 do
 root -b -q -l TMVARegressionApplication.C\(${e},${df}\)
 done
